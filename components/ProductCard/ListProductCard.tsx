ce645364117e0e03e2d4de1f45ee4302c4803fcc
Arnob Mahmud <arnob_t78@yahoo.com>
2025-08-29 13:36:32 +0200

    update product grid

--- components/ProductCard/ListProductCard.tsx
+++ components/ProductCard/ListProductCard.tsx
@@ -48,7 +48,8 @@ const ProductGrid: React.FC<ProductGridProps> = ({
   handleAddToCart,
   onCardClick,
 }) => (
-  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-2 gap-y-2 sm:gap-x-4 sm:gap-y-4 justify-items-center w-full max-w-7xl pt-4 sm:pt-4">
+  // <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-2 gap-y-2 sm:gap-x-4 sm:gap-y-4 justify-items-center w-full max-w-7xl pt-4 sm:pt-4">
+  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-0 gap-y-2 sm:gap-x-4 sm:gap-y-4 justify-items-center w-full max-w-7xl pt-4 sm:pt-4">
     {products.length > 0 ? (
       products.map((product, idx) => (
         <ListProductCardItem
